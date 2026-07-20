9660b8e296139b62ad54a177e2ad8a6fd3dfc3b5
bogdanbogachov <bogdan@axya.co>
2026-07-20 18:52:35 -0400

    Bigger CQAD DS

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-user=bogdan.bogachov@mail.mcgill.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-adml2021
-#SBATCH --time=06:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
@@ -23,4 +23,4 @@ source ENV/bin/activate
 export OPENAI_API_KEY="dummy"
 
 # Run the Python script
-python main.py --infer_slg=True --router="finetuned"
+python main.py --finetune=True --infer_finetuned=True --infer_slg=True --router="finetuned"
